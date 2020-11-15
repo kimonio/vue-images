f21fe9a6aa7138d60cf9225a06d51c9ac1136e06
Antonio Cunanan <antonio.cunanan@gmail.com>
2020-11-15 16:13:39 -0600

    Add navigation bar.

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="ui secondary pointing menu">
+      <a href="/" class="active item">
+        Vue Images
+      </a>
+      <div class="right menu">
+        <a class="item">
+          Gallery
+        </a>
+        <a class="item">
+          Upload
+        </a>
+        <a class="item">
+          Logout
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
